76223fe3ec384b79d9e0d9238b3491653a6e8104
David Hughes <david.hughes125@gmail.com>
2014-06-09 20:52:20 +0100

    Drop debug questions & var_dumps

--- app/views/meltdown/ti4quiz.blade.php
+++ app/views/meltdown/ti4quiz.blade.php
@@ -1,7 +1,14 @@
 <form method="POST" action="{{ url('meltdown-london/ti4-quiz/submit') }}" accept-charset="UTF-8">
-  {{ Form::label('ti3-victor', 'Who won TI3?') }}
-  {{ Form::radio('ti3-victor', 'Alliance') }} Alliance
-  {{ Form::radio('ti3-victor', 'Na`Vi') }} Na`Vi
+
+  <?php
+  foreach ($quiz as $question) {
+    echo Form::label($question['html-name'], $question['question']);
+
+    foreach ($question['answers'] as $index=>$answer) {
+      echo Form::radio($question['html-name'], $answer) . $answer;
+    }
+  }
+  ?>
 
   {{ Form::submit('Done!') }}
 </form>
